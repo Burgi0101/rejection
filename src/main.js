34c90d0a144288a182c3df1b5c21b69163e0b638
Georg Burgstaller <gburgstaller@greentube.com>
2017-03-01 13:52:04 +0100

    getting stored data onload now added error handling for addQuestion

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@
 const createQuestion = (data) => Object.assign({}, {
     timestamp: new Date().toLocaleString(),
     question: "",
-    status: 'asked'
+    status: ""
 }, data);
+
+
+function getStoredData() {
+    const existingQuestions = JSON.parse(localStorage.getItem('allquestions')) || [];
+    document.getElementById("questions").innerHTML = existingQuestions.map((obj) => {
+        return obj.question + "</br>";
+    });
+}
 
 // creates a new question object and adds it to the localStorage
 function addQuestion() {
@@ -11,9 +19,10 @@ function addQuestion() {
 
     if (currentQuestion !== '' && currentQuestion.length >= 6) {
 
+        document.getElementById("errors").innerText = "";
         const existingQuestions = JSON.parse(localStorage.getItem('allquestions')) || [];
 
-        const questionObj = createQuestion({ question: currentQuestion });
+        const questionObj = createQuestion({ question: currentQuestion, status: 'asked' });
         existingQuestions.push(questionObj);
 
         localStorage.setItem('allquestions', JSON.stringify(existingQuestions));
@@ -21,8 +30,12 @@ function addQuestion() {
             return obj.question + "</br>";
         });
     }
+    else {
+        document.getElementById("errors").innerText = "Your question has to be at least 6 characters long!";
+    }
 }
 
 function clearStorage() {
     localStorage.clear();
+    document.getElementById("questions").innerHTML = "";
 }
