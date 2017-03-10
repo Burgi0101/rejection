83664fc4f1ac6fd344a41230319744ee1c6ec0b1
Georg Burgstaller <georg.burgstaller@gmx.at>
2017-03-10 09:46:19 +0100

    layout changes

--- src/main.js
+++ src/main.js
@@ -1,21 +1,28 @@
-// question factory
-const createQuestion = (data) => Object.assign({}, {
+//factories
+const createQuestionObject = (data) => Object.assign({}, {
     timestamp: new Date().toLocaleString(),
     question: "",
     status: ""
 }, data);
 
+const createQuestion = (arr) => {
+    document.getElementById("questions").innerHTML = arr.map((obj, index) => {
+        return '<div> Question ' + index + ': ' + obj.question +
+            ' <button onclick="answerQuestion(rejected)">rejected</button>' +
+            ' <button onclick="answerQuestion(accepted)">accepted</button></div></br>';
+    }).join(" ");
+};
+
+//###########################################################################
 
 function getStoredData() {
     const existingQuestions = JSON.parse(localStorage.getItem('allquestions')) || [];
-    document.getElementById("questions").innerHTML = existingQuestions.map((obj) => {
-        return "<div>" + obj.question + "</div>";
-    }).join(" ");
+    createQuestion(existingQuestions);
 }
 
 // creates a new question object and adds it to the localStorage
 function addQuestion() {
-    let currentQuestion = document.getElementById('question');
+    let currentQuestion = document.getElementById('askQuestion');
 
     if (currentQuestion.value !== '' && currentQuestion.value.length >= 6) {
 
@@ -26,18 +33,16 @@ function addQuestion() {
         const existingQuestions = JSON.parse(localStorage.getItem('allquestions')) || [];
 
         // create new question object with the current value of the input field
-        const questionObj = createQuestion({ question: currentQuestion.value , status: 'asked' });
-        
+        const newQuestionObject = createQuestionObject({ question: currentQuestion.value, status: 'asked' });
+
         //add it to the already existing questions
-        existingQuestions.push(questionObj);
+        existingQuestions.push(newQuestionObject);
 
         // push the whole array (old questions + current) to the local storage again
         localStorage.setItem('allquestions', JSON.stringify(existingQuestions));
 
         // create a div for every question and enter the question as text
-        document.getElementById("questions").innerHTML = existingQuestions.map((obj) => {
-            return "<div>" + obj.question + "</div>";
-        }).join(" ");
+        createQuestion(existingQuestions);
 
         // clear the input field if the question was added
         currentQuestion.value = "";
